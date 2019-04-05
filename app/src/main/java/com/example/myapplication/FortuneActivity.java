a2890467cf93a80952519e605b634abd642e4781
J Hershey <hersheyj32@gmail.com>
2019-04-05 13:35:59 -0700

    okay this seem to work

--- app/src/main/java/com/example/myapplication/FortuneActivity.java
+++ app/src/main/java/com/example/myapplication/FortuneActivity.java
@@ -8,6 +8,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,42 +16,58 @@ import android.widget.Toast;
 import com.example.myapplication.bluetooth.BluetoothService;
 import com.example.myapplication.bluetooth.PrinterCommand;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+import static com.example.myapplication.BuildConfig.DEBUG;
 import static com.example.myapplication.MainActivity.FORTUNE;
 
 public class FortuneActivity extends AppCompatActivity {
 
-    // Name of the connected device
-    private String mConnectedDeviceName = null;
-    // Local Bluetooth adapter
-    private BluetoothAdapter mBluetoothAdapter = null;
-    // Member object for the services
-    private BluetoothService mService = null;
-    /******************************************************************************************************/
-    // Message types sent from the BluetoothService Handler
-    public static final int MESSAGE_STATE_CHANGE = 1;
-    public static final int MESSAGE_READ = 2;
-    public static final int MESSAGE_WRITE = 3;
-    public static final int MESSAGE_DEVICE_NAME = 4;
-    public static final int MESSAGE_TOAST = 5;
-    public static final int MESSAGE_CONNECTION_LOST = 6;
-    public static final int MESSAGE_UNABLE_CONNECT = 7;
 
-    // Intent request codes
-    private static final int REQUEST_CONNECT_DEVICE = 1;
-    private static final int REQUEST_ENABLE_BT = 2;
-    private static final int REQUEST_CHOSE_BMP = 3;
-    private static final int REQUEST_CAMER = 4;
+    // android built in classes for bluetooth operations
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
 
-    private static final String CHINESE = "GBK";
-    private static final String THAI = "CP874";
-    private static final String KOREAN = "EUC-KR";
-    private static final String BIG5 = "BIG5";
+    // needed for communication to bluetooth device / network
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
 
+    byte[] readBuffer;
+    int readBufferPosition;
+    volatile boolean stopWorker;
 
-    // Key names received from the BluetoothService Handler
-    public static final String DEVICE_NAME = "device_name";
-    public static final String TOAST = "toast";
+    TextView myLabel;
+    Button sendButton;
 
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch (msg.arg1) {
+                case BluetoothService.STATE_CONNECTED:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,96 +76,187 @@ public class FortuneActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String fortune = intent.getStringExtra(FORTUNE);
         TextView textView = findViewById(R.id.FortuneTextView);
+        myLabel = (TextView) findViewById(R.id.label);
+        sendButton= (Button) findViewById(R.id.send);
         textView.setText(fortune);
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            Toast.makeText(this, "Bluetooth is not available",
-                    Toast.LENGTH_LONG).show();
-            finish();
+
+        try {
+            // more codes will be here
+            findBT();
+            openBT();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableIntent = new Intent(
-                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-            // Otherwise, setup the session
-        } else {
-            if (mService == null)
-                KeyListenerInit();//监听
-        }
-    }
-//
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//
-//        // If Bluetooth is not on, request that it be enabled.
-//        // setupChat() will then be called during onActivityResult
-//        if (!mBluetoothAdapter.isEnabled()) {
-//            Intent enableIntent = new Intent(
-//                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-//            // Otherwise, setup the session
-//        } else {
-//            if (mService == null)
-//                KeyListenerInit();//监听
-//        }
-//    }
 
-    private void KeyListenerInit() {
-        mService = new BluetoothService(this, mHandler);
-    }
-
-    @SuppressLint("HandlerLeak")
-    private final Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case MESSAGE_STATE_CHANGE:
-                    switch (msg.arg1) {
-                        case BluetoothService.STATE_CONNECTED:
-
-                            Print_Test();//
-
-                            break;
-                        case BluetoothService.STATE_CONNECTING:
-                            break;
-                        case BluetoothService.STATE_LISTEN:
-                        case BluetoothService.STATE_NONE:
-                            break;
-                    }
-                    break;
-                case MESSAGE_WRITE:
-
-                    break;
-                case MESSAGE_READ:
-
-                    break;
-                case MESSAGE_DEVICE_NAME:
-                    // save the connected device's name
-                    mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
-                    Toast.makeText(getApplicationContext(),
-                            "Connected to " + mConnectedDeviceName,
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                case MESSAGE_TOAST:
-                    Toast.makeText(getApplicationContext(),
-                            msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
-                            .show();
-                    break;
-                case MESSAGE_CONNECTION_LOST:    //蓝牙已断开连接
-                    Toast.makeText(getApplicationContext(), "Device connection was lost",
-                            Toast.LENGTH_SHORT).show();
-
-                    break;
-                case MESSAGE_UNABLE_CONNECT:     //无法连接设备
-                    Toast.makeText(getApplicationContext(), "Unable to connect device",
-                            Toast.LENGTH_SHORT).show();
-                    break;
+        // send data typed by the user to be printed
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    sendData();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
+        });
+    }
+
+    private void sendData() throws IOException{
+        try {
+
+            // the text typed by the user
+            String msg = "to print";
+            msg += "\n";
+
+            mmOutputStream.write(msg.getBytes());
+
+            // tell the user data were sent
+            myLabel.setText("Data sent.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    };
+    }
+
+    private static final UUID MY_UUID = UUID
+            .fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private void openBT() {
+        try {
+
+            // Standard SerialPortService ID
+
+//            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(MY_UUID);
+//            mmSocket.getInputStream().close();
+//            mmSocket.getOutputStream().close();
+//            mmSocket.close();
+
+            mmSocket.connect();
+            mmOutputStream = mmSocket.getOutputStream();
+            mmInputStream = mmSocket.getInputStream();
+
+            beginListenForData();
+
+            myLabel.setText("Bluetooth Opened");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * after opening a connection to bluetooth printer device,
+     * we have to listen and check if a data were sent to be printed.
+     */
+    void beginListenForData() {
+        try {
+            final Handler handler = new Handler();
+
+            // this is the ASCII code for a newline character
+            final byte delimiter = 10;
+
+            stopWorker = false;
+            readBufferPosition = 0;
+            readBuffer = new byte[1024];
+
+            workerThread = new Thread(new Runnable() {
+                public void run() {
+
+                    while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+
+                        try {
+
+                            int bytesAvailable = mmInputStream.available();
+
+                            if (bytesAvailable > 0) {
+
+                                byte[] packetBytes = new byte[bytesAvailable];
+                                mmInputStream.read(packetBytes);
+
+                                for (int i = 0; i < bytesAvailable; i++) {
+
+                                    byte b = packetBytes[i];
+                                    if (b == delimiter) {
+
+                                        byte[] encodedBytes = new byte[readBufferPosition];
+                                        System.arraycopy(
+                                                readBuffer, 0,
+                                                encodedBytes, 0,
+                                                encodedBytes.length
+                                        );
+
+                                        // specify US-ASCII encoding
+                                        final String data = new String(encodedBytes, "US-ASCII");
+                                        readBufferPosition = 0;
+
+                                        // tell the user data were sent to bluetooth printer device
+                                        handler.post(new Runnable() {
+                                            public void run() {
+                                                myLabel.setText(data);
+                                            }
+                                        });
+
+                                    } else {
+                                        readBuffer[readBufferPosition++] = b;
+                                    }
+                                }
+                            }
+
+                        } catch (IOException ex) {
+                            stopWorker = true;
+                        }
+
+                    }
+                }
+            });
+
+            workerThread.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void findBT() {
+
+        try {
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            if (mBluetoothAdapter == null) {
+                myLabel.setText("No bluetooth adapter available");
+            }
+
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBluetooth, 0);
+            }
+
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice device : pairedDevices) {
+
+                    Log.e("DEVICE NAME", device.getName());
+
+                    // RPP300 is the name of the bluetooth printer device
+                    // we got this name from the list of paired devices
+                    if (device.getName().contains("BlueTooth Printer")) {
+                        mmDevice = device;
+                        break;
+                    }
+                }
+            }
+
+            myLabel.setText("Bluetooth device found.");
+            mBluetoothAdapter.cancelDiscovery();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public void toPrint(View view) {
         Context context = getApplicationContext();
@@ -159,32 +267,6 @@ public class FortuneActivity extends AppCompatActivity {
         toast.show();
 //        Print_Test();
     }
-    /****************************************************************************************************/
-    /**
-     * 连接成功后打印测试页
-     */
-    private void Print_Test() {
 
-        String msg = "Congratulations!\n\n";
-        String data = "You succeeded in achieving stage 1 of software driven relationship\n";
 
-        SendDataByte(PrinterCommand.POS_Print_Text(msg, CHINESE, 0, 1, 1, 0));
-        SendDataByte(PrinterCommand.POS_Print_Text(data, CHINESE, 0, 0, 0, 0));
-        SendDataByte(PrinterCommand.POS_Set_Cut(1));
-        SendDataByte(PrinterCommand.POS_Set_PrtInit());
-
-    }
-
-    /*
-     *SendDataByte
-     */
-    private void SendDataByte(byte[] data) {
-
-        if (mService.getState() != BluetoothService.STATE_CONNECTED) {
-            Toast.makeText(this, "Not Connected", Toast.LENGTH_SHORT)
-                    .show();
-            return;
-        }
-        mService.write(data);
-    }
 }
