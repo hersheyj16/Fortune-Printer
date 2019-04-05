b0decc156abaf88cb64e8fb888bbca6e7a7a9d11
J Hershey <hersheyj32@gmail.com>
2019-04-04 21:35:44 -0700

    this is confusing

--- app/src/main/java/com/example/myapplication/FortuneActivity.java
+++ app/src/main/java/com/example/myapplication/FortuneActivity.java
@@ -1,27 +1,154 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.bluetooth.BluetoothService;
+import com.example.myapplication.bluetooth.PrinterCommand;
+
 import static com.example.myapplication.MainActivity.FORTUNE;
 
 public class FortuneActivity extends AppCompatActivity {
+
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+    // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
+    // Member object for the services
+    private BluetoothService mService = null;
+    /******************************************************************************************************/
+    // Message types sent from the BluetoothService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    public static final int MESSAGE_CONNECTION_LOST = 6;
+    public static final int MESSAGE_UNABLE_CONNECT = 7;
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+    private static final int REQUEST_CHOSE_BMP = 3;
+    private static final int REQUEST_CAMER = 4;
+
+    private static final String CHINESE = "GBK";
+    private static final String THAI = "CP874";
+    private static final String KOREAN = "EUC-KR";
+    private static final String BIG5 = "BIG5";
+
+
+    // Key names received from the BluetoothService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fortune);
-
         Intent intent = getIntent();
         String fortune = intent.getStringExtra(FORTUNE);
         TextView textView = findViewById(R.id.FortuneTextView);
         textView.setText(fortune);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the session
+        } else {
+            if (mService == null)
+                KeyListenerInit();//监听
+        }
     }
+//
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//
+//        // If Bluetooth is not on, request that it be enabled.
+//        // setupChat() will then be called during onActivityResult
+//        if (!mBluetoothAdapter.isEnabled()) {
+//            Intent enableIntent = new Intent(
+//                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+//            // Otherwise, setup the session
+//        } else {
+//            if (mService == null)
+//                KeyListenerInit();//监听
+//        }
+//    }
+
+    private void KeyListenerInit() {
+        mService = new BluetoothService(this, mHandler);
+    }
+
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothService.STATE_CONNECTED:
+
+                            Print_Test();//
+
+                            break;
+                        case BluetoothService.STATE_CONNECTING:
+                            break;
+                        case BluetoothService.STATE_LISTEN:
+                        case BluetoothService.STATE_NONE:
+                            break;
+                    }
+                    break;
+                case MESSAGE_WRITE:
+
+                    break;
+                case MESSAGE_READ:
+
+                    break;
+                case MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                    Toast.makeText(getApplicationContext(),
+                            "Connected to " + mConnectedDeviceName,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    Toast.makeText(getApplicationContext(),
+                            msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
+                            .show();
+                    break;
+                case MESSAGE_CONNECTION_LOST:    //蓝牙已断开连接
+                    Toast.makeText(getApplicationContext(), "Device connection was lost",
+                            Toast.LENGTH_SHORT).show();
+
+                    break;
+                case MESSAGE_UNABLE_CONNECT:     //无法连接设备
+                    Toast.makeText(getApplicationContext(), "Unable to connect device",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
 
     public void toPrint(View view) {
         Context context = getApplicationContext();
@@ -30,5 +157,34 @@ public class FortuneActivity extends AppCompatActivity {
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+//        Print_Test();
+    }
+    /****************************************************************************************************/
+    /**
+     * 连接成功后打印测试页
+     */
+    private void Print_Test() {
+
+        String msg = "Congratulations!\n\n";
+        String data = "You succeeded in achieving stage 1 of software driven relationship\n";
+
+        SendDataByte(PrinterCommand.POS_Print_Text(msg, CHINESE, 0, 1, 1, 0));
+        SendDataByte(PrinterCommand.POS_Print_Text(data, CHINESE, 0, 0, 0, 0));
+        SendDataByte(PrinterCommand.POS_Set_Cut(1));
+        SendDataByte(PrinterCommand.POS_Set_PrtInit());
+
+    }
+
+    /*
+     *SendDataByte
+     */
+    private void SendDataByte(byte[] data) {
+
+        if (mService.getState() != BluetoothService.STATE_CONNECTED) {
+            Toast.makeText(this, "Not Connected", Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+        mService.write(data);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/bluetooth/Main_Activity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication.bluetooth;
+
+class Main_Activity {
+    /******************************************************************************************************/
+    // Debugging
+    private static final String TAG = "Main_Activity";
+    private static final boolean DEBUG = true;
+    /******************************************************************************************************/
+    // Message types sent from the BluetoothService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    public static final int MESSAGE_CONNECTION_LOST = 6;
+    public static final int MESSAGE_UNABLE_CONNECT = 7;
+    /*******************************************************************************************************/
+    // Key names received from the BluetoothService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+}
