7769bc9a1988551de3c032b2cbcc8da6572e588b
J Hershey <hersheyj32@gmail.com>
2019-04-04 20:33:33 -0700

    toast working

--- app/src/main/java/com/example/myapplication/FortuneActivity.java
+++ app/src/main/java/com/example/myapplication/FortuneActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,9 +23,12 @@ public class FortuneActivity extends AppCompatActivity {
         textView.setText(fortune);
     }
 
-    public void toPrint() {
-        Toast toast=Toast.makeText(getApplicationContext(),"Hello Printing Stub",Toast.LENGTH_SHORT);
-        toast.setMargin(50,50);
+    public void toPrint(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
         toast.show();
     }
 }
